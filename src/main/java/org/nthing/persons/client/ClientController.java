6d242e0924a7605c3c5e82f1e501d9e4c8264cc9
Nthingtech <67294308+Nthingtech@users.noreply.github.com>
2024-09-27 20:43:54 -0300

    feat:Client Update method

--- src/main/java/org/nthing/persons/client/ClientController.java
+++ src/main/java/org/nthing/persons/client/ClientController.java
@@ -7,6 +7,7 @@ import jakarta.validation.constraints.Positive;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.WebApplicationException;
@@ -34,7 +35,7 @@ public class ClientController {
 
     @GET
     @Path("{id}")
-    public Client findById(@Positive @NotNull Long id) {
+    public Client findByIdClient(@Positive @NotNull Long id) {
         Client client = Client.findById(id);
         if (client == null || client.id <= 0) {
             throw new WebApplicationException("Client with id of " + id + " does not exist", 404);
@@ -51,6 +52,36 @@ public class ClientController {
         }
         Client createdClient = clientService.createClient(newClient);
         return Response.status(Response.Status.CREATED).entity(createdClient).build() ;
+    }
+
+    @PUT
+    @Transactional
+    @Path("{id}")
+    public Client updateClient(Long id, Client client){
+        Client existingClient = Client.findById(id);
+
+        if (existingClient ==null) {
+            throw new WebApplicationException("Client whit id of " + id + " does not exist", 404);
+        }
+
+        existingClient.name.firstName = client.name.firstName;
+        existingClient.name.lastName = client.name.lastName;
+        existingClient.birthDate = client.birthDate;
+        existingClient.cpf = client.cpf;
+        existingClient.gender = client.gender;
+        existingClient.address.residenceNumber = client.address.residenceNumber;
+        existingClient.address.street = client.address.street;
+        existingClient.address.district = client.address.street;
+        existingClient.address.city = client.address.city;
+        existingClient.address.state = client.address.state;
+        existingClient.address.zipcode = client.address.zipcode;
+        existingClient.address.complement = client.address.complement;
+        existingClient.phone = client.phone;
+        existingClient.email = client.email;
+        existingClient.password = client.password;
+        existingClient.instagram = client.instagram;
+
+        return existingClient;
     }
 
 
